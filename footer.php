d62472f64ef7366d68034e4e436f6ecdede73312
iqbal rifai <iqbalrifai@noreply.example.org>
2018-06-14 02:24:51 +0200

    Add 'footer.php'

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer" style="margin-top: 15px;background: #f5f5f5;padding: 15px 0;">
+	<div class="container">
+		<p class="text-muted">&copy; <?php echo config('site.copyright');?></p>
+	</div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<?php echo analytics();?>
+</body>
+</html>
