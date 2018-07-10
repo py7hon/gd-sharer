32e6c4656060e67db16bc15fef96e6e612c1a095
Muhammad Iqbal Rifai <iqbalrifai@waifu.club>
2018-07-10 14:43:10 +0700

    Add Some Stuff

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
-<footer class="footer" style="margin-top: 15px;background: #f5f5f5;padding: 15px 0;">
+<footer class="footer">
 	<div class="container">
-		<p class="text-muted">&copy; <?php echo config('site.copyright');?></p>
+		<p class="text-muted">&copy; <?php echo config('site.copyright');?><a href="mailto:abuse@0wo.me"> Contact/BUG Report</a></p>
 	</div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vuesax/dist/vuesax.umd.js"></script>
+<script src="http://0wo.me/0wo.js" integrity="sha384-Tc5Iswqib027qvyjSMfHjOMa3211LkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <?php echo analytics();?>
 </body>
 </html>
