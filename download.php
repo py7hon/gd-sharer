b2aff1ae9a3d315008d8f2c9ebf564aed2928704
Muhammad Iqbal Rifai <iqbalrifai@waifu.club>
2018-07-10 14:39:10 +0700

    Sorry Wrong Update File :smile:

--- download.php
+++ download.php
@@ -1,255 +1,180 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php 
+<?php
 
-if(!empty($title)) {
-	
-	echo "<title>$title</title>";
-	
-} else {
-	
-	echo "<title>404</title>";
-}
+error_reporting(0);
+session_start();
 
-
-echo "
-<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />
-<link href=\"https://cdn.jsdelivr.net/npm/vuesax/dist/vuesax.css\" rel=\"stylesheet\">
-<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">
-<style media=\"screen\">.content-wrap{margin:0 auto;padding:2em 15px 5em;max-width:1000px}
-	.page-header{margin:0 0 20px 0;}
-	img {max-width: 100%;height: auto;}
-	.navnav{background-color:#F8F8FF; border:none}
-	.navnav a{color:black !important}
-	.navnav a:hover{color:rgb(158, 158, 158) !important}
-	.jumbotron{background-color:#F8F8FF;border:8px solid #eee}
-body{background-color:#FFF}
-.form-group {
-margin-bottom: 0;
-padding: 10px 0;
-}
-.form-group:first-child {
-border-color: transparent;
-}
-
-.form-control {
--webkit-box-shadow: none;
-box-shadow: none;
-border-width: 2px;
-min-height: 40px;
-height: auto;
-}
-.form-control:focus {
--webkit-box-shadow: none;
-box-shadow: none;
-}
-
-.form-vertical {
-counter-reset: fieldset;
-}
-.form-vertical fieldset {
-padding-top: 10px;
-margin: 50px 0;
-}
-.form-vertical fieldset > legend:before {
-content: counter(fieldset);
-counter-increment: fieldset;
-position: absolute;
-left: -25px;
-width: 30px;
-height: 30px;
-line-height: 30px;
-border-radius: 15px;
-text-align: center;
-background: #428bca;
-color: white;
-font-size: 75%;
-font-weight: bold;
-}
-
-label.checkbox {
-margin-bottom: 15px;
-position: relative;
-}
-label.checkbox .icheckbox_square-blue {
-position: absolute;
-top: 0;
-left: 0;
-}
-label.checkbox input {
-position: absolute;
-left: 0;
-top: 0;
-}
-label.checkbox span {
-padding-left: 35px;
-display: block;
-}
-
-.radio label {
-padding-left: 0;
-}
-.radio span {
-vertical-align: middle;
-margin-left: 5px;
-}
-
-.btn {
-height: 40px;
-padding: 10px 16px;
-border-radius: 3px;
-min-width: 80px;
-}
-
-.btn-group.radio-group .btn {
-height: 50px;
-line-height: 22px;
-padding: 12px 20px;
-}
-.btn-group.radio-group .btn span {
-line-height: 22px;
-vertical-align: middle;
-margin-left: 5px;
-}
-
-/* bootstrap select styles */
-.bootstrap-select .btn {
-min-height: 40px;
-border-width: 2px;
--webkit-box-shadow: none;
-box-shadow: none;
-outline: 0;
-}
-.bootstrap-select .btn:hover {
-background: white;
-}
-
-.bootstrap-select .btn:focus,
-.bootstrap-select.btn-group.open .dropdown-toggle {
-border-color: #428bca;
-background: white;
-outline: 0;
-}
-
-.bootstrap-select .btn.bootstrap-select.btn-group.open .dropdown-toggle {
-border-color: #428bca;
-color: white;
--webkit-box-shadow: none;
-box-shadow: none;
-}
-
-.dropdown-menu > li > a:hover,
-.dropdown-menu > li > a:focus {
-background: #428bca;
-color: white;
-}
-
-label {
-cursor: pointer;
-}
-
-:-ms-input-placeholder {
-color: #ccc;
-}
-
-::-moz-placeholder {
-color: #ccc;
-}
-
-::-webkit-input-placeholder {
-color: #ccc;
-}
-
-</style>
-".head_content();
+include "system/function.php";
 
 if($_GET[id]) {
 
-	$headtag = config('head.tag');
-	if(!empty($headtag)) {
+	$share = json_decode(file_get_contents("base/data/main/share/$_GET[id].json"), true);
+	$users = json_decode(file_get_contents("base/data/user/".$share[file][user_id].".json"),true);
+	$kontol = $share[file][file_id];
+	if(isset($share[file][title])) {
 
-		echo $headtag;
+		$title = $share[file][title]." - ".formatBytes($share[file][size]);
+
+	} else {
+
+		$title = "File not found :(";
 
 	}
-	echo "<meta name=\"robots\" content=\"noindex\" />";
 
-}
+	include "header.php";
 
-echo "
-	</head>
-	<body>
-		<nav cclass=\"navbar navbar-default navbar-static-top navnav\">
-			<div class=\"container\">
-			<div class=\"navbar-header\">
-				<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
-					<span class=\"sr-only\">Toggle navigation</span>
-					<span class=\"icon-bar\"></span>
-					<span class=\"icon-bar\"></span>
-					<span class=\"icon-bar\"></span>
-				</button>
-				<a class=\"navbar-brand\" href=\"http://".config('site.domain')."/\">".config('site.tag')."</a>
-			</div>
-			<div id=\"navbar\" class=\"navbar-collapse collapse\">";
-		
-if($_SESSION[email]) {
+	echo "<div class=\"container\"><div class=\"content-wrap\">";
 
-	$file = file_get_contents("base/data/user/$_SESSION[email].json");
-	$user = json_decode($file, true);
+	if($share[file][poster]) {
 
-	echo "<ul class=\"nav navbar-nav\">";
-			
-	if($user[role] == "admin") {
-				
-		echo "
-			<li class=\"dropdown\">
-				<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\"><span class=\"glyphicon glyphicon-cog\" aria-hidden=\"true\"></span> Setting <span class=\"caret\"></span></a>
-				<ul class=\"dropdown-menu\">
-					<li><a href=\"/menu.php?s=setting&a=site\">Site</a></li>
-					<li><a href=\"/menu.php?s=setting&a=oauth\">OAuth 2.0</a></li>
-					<li role=\"separator\" class=\"divider\"></li>
-					<li class=\"dropdown-header\">Page</li>
-					<li><a href=\"/menu.php?s=setting&a=page\">Index</a></li>
-					<li role=\"separator\" class=\"divider\"></li>
-					<li class=\"dropdown-header\">Advertisement</li>
-					<li><a href=\"/menu.php?s=setting&a=ads&t=banner\">Banner</a></li>
-					<li><a href=\"/menu.php?s=setting&a=ads&t=headtag\">Head Tag</a></li>
-				</ul>
-			</li>
+		$poster = $share[file][poster];
 
-			<li>
-				<a href=\"/menu.php?s=backup\"><span class=\"glyphicon glyphicon-floppy-save\" aria-hidden=\"true\"></span> Backup</a>
-			</li>";
-				
 	}
-			
-	echo "
-		<li>
-			<a href=\"/menu.php?s=share\"><span class=\"glyphicon glyphicon-link\" aria-hidden=\"true\"></span> Share</a>
-		</li>
-		<li>
-			<a href=\"/menu.php?s=file\"><span class=\"glyphicon glyphicon-file\" aria-hidden=\"true\"></span> File</a>
-		</li>
-		<li>
-			<a href=\"/menu.php?s=account\"><span class=\"glyphicon glyphicon-user\" aria-hidden=\"true\"></span> Account</a>
-		</li>
-		<li>
-			<a href=\"/login.php?action=logout\"><span class=\"glyphicon glyphicon-log-out\" aria-hidden=\"true\"></span> Logout</a>
-		</li>";
-			
-} else {
 
-	echo "
-		<ul class=\"nav navbar-nav navbar-right\">
-		<li>
-			<a href=\"/login.php\">Login</a>
-		</li>
-		<li>
-		<a href=\"/https://accounts.google.com/SignUp?hl=in\">Register</a>
-		</li>";
+	if($_GET[code] == true) {
 
+		include "system/menu/code.php";
+
+	} 
+
+	else {
+
+		if(isset($_POST[submit])) {
+
+			$ruse = token("refresh",$_SESSION[email]);
+			$copy = copyfile($share[file][file_id],$ruse[access_token]);
+			anyone($copy[id],$ruse[access_token]);
+							
+			if($copy[id]) {
+								
+				echo "
+					<meta http-equiv=\"refresh\" content=\"5;URL='https://drive.google.com/file/d/$copy[id]/view'\" />
+					<div style=\"padding: 50px 15px;text-align: center;background-color: #fff;\">
+						<h2>Thank you for downloading!</h2><br />
+						If your download doesn't start automatically within a few seconds, <a href=\"https://drive.google.com/file/d/$copy[id]/view\">click here</a>.
+					</div>";
+								
+			} else {
+
+				echo "<div style=\"padding: 50px 15px;text-align: center;background-color: #fff;\">";
+
+				if($copy[error][message] == "The user's Drive storage quota has been exceeded.") {
+
+					echo "Your Google Drive account was full, please delete some files before genereate link download.";
+
+				} else if($copy[error][message] == "User rate limit exceeded.") {
+
+					echo "Your Google Drive account was limited, please login with another Google account.<br>
+							The account will be returned to normal after 24 hours.";
+
+				} else if($copy[error][message] == "Forbidden") {
+
+					echo "File cannot be opened :(";
+					
+				} else {
+
+					echo $copy[error][message];
+
+				}
+
+				echo "</div>";
+			
+			} 
+
+		}
+
+		else if($share[file][status] == "publish") {
+					
+			/*echo "
+				<div style=\"background-color: #000;display: block;\">
+				<div style=\"max-height:400px;max-width:800px;margin-left: auto; margin-right: auto;\">
+				";
+
+			if($poster) {
+
+				echo "
+					<div style=\"text-align: center;\">
+						<img src=\"$poster\" width=\"400px\">
+					</div>";
+
+			} else {
+
+				echo "
+					<div style=\"color: #fff;text-align: center;padding: 30px 15px;\">
+						No preview available
+					</div>";
+
+			}
+
+			echo "</div></div>";*/
+			$banner = config('banner.1');
+			
+			if(!empty($banner)) { 
+
+				echo "<div style=\"text-align: center;\">$banner</div>";
+
+			}
+
+			echo "
+				<center><iframe src='https://drive.0wo.me/player?id=$kontol'frameborder='0' width='480' height='240'></iframe></center>
+				<textarea class=\"form-control\">&lt;iframe src=\"https://drive.0wo.me/player?id=$kontol\" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=\"640\" HEIGHT=\"350\" allowfullscreen=\"true\"&gt;&lt;/iframe&gt;
+</textarea>
+<style type=\"text/css\">
+			.fileinfo {line-height: 21px;color:eee;font-weight: 700;padding: 10px;background-color:#ebebeb;border: 3px solid #eee;border-radius: 5px;margin: 10px 20px;}
+			.fileinfo span{display: block;}
+			.bysize{text-align: center;margin: 10px 20px;}
+			</style>
+				<div class=\"fileinfo\">
+								<span>Filename	:".$share[file][title]." </span>
+								<span>Format    :".$share[file][format]."</span>
+								<span>Size		:".formatBytes($share[file][size])."</span>
+					</div>
+			";
+
+			$banner = config('banner.2');
+			
+			if(!empty($banner)) { 
+
+				echo "<div style=\"text-align: center;padding-bottom: 20px;\">$banner</div>";
+
+			}
+
+
+			if($_SESSION[email]) {
+
+				echo "
+					<div style=\"text-align: center;\">
+						<form method=\"post\">
+							<button class=\"btn btn-primary\" type=\"submit\" name=\"submit\">Download File</button>
+						</form>
+					</div>";
+						
+			} else {
+
+				echo "
+					<div class=\"alert alert-danger\" role=\"alert\">
+						You Need to login first before start download file in this web.
+					</div>";
+
+				echo "
+					<div style=\"text-align: center;\">
+						<form action=\"/login.php\" method=\"GET\">
+							<input name=\"r\" value=\"http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]\" type=\"hidden\">
+								<button class=\"btn btn-primary\" type=\"submit\">Login with Google</button>
+						</form>
+					</div>";
+
+			}
+					
+		} else echo "
+					<div style=\"padding: 50px 15px;text-align: center;background-color: #fff;\">
+						<h2>File not found :(</h2>
+					</div>";
+
+	}
+
+
+	echo "</div></div>";
+	
 }
 
-echo "</ul></div></div></nav>";
-
-?>
+include "footer.php";
