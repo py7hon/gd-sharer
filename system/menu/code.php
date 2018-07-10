bde77af5c76c3e4ca49a3f5e78d873d8481d5c0d
Muhammad Iqbal Rifai <iqbalrifai@waifu.club>
2018-07-10 14:32:05 +0700

    Delete Embed and Move to Download page

--- system/menu/code.php
+++ system/menu/code.php
@@ -4,7 +4,7 @@ if($_GET[id]) {
 }
 
 echo "
-	<div class=\"page-header\"><h1>".$share[file][title]." - ".formatBytes($share[file][size])."</h1></div>
+	<div class=\"page-header\"><h2 style=\"text-align:center\">".$share[file][title]." - ".formatBytes($share[file][size])."</h2></div>
 
 	<div class=\"form-horizontal\">
 
@@ -27,13 +27,7 @@ echo "
 			<div class=\"col-sm-10\">
 				<input class=\"form-control\" value=\"&lt;a href=&quot;http://".config('site.domain')."/file/$_GET[id]&quot;&gt;".$share[file][title]." - ".formatBytes($share[file][size])."&lt;/a&gt;\">
 			</div>
-		
-        <div class=\"form-group\">
-			<label class=\"col-sm-2 control-label\" style=\"text-align: left;\">Embed Player</label>
-			<div class=\"col-sm-10\">
-				<input class=\"form-control\" value=\"&lt;iframe=&quot;https://drive.0wo.me/player?id=$kontol frameborder=&quot;0&quot; width=&quot;480&quot; height=&quot;240&quot;&lt;/frame&gt;\">
-			</div>
-		</div>
+		<a class=\"btn btn-info\" style=\"text-align:right\" href=\"http://".config('site.domain')."/file/$_GET[id]\">Download Page</a>
         </div>
 	</div>
 
